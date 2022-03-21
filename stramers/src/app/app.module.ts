09ddd61118ea2bd26d84de07eadb1764fa4029e1
Terminator-True <jf28renault@gmail.com>
2022-03-21 20:54:05 +0100

    002-Joel

--- stramers/src/app/app.module.ts
+++ stramers/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasegameComponent } from './basegame/basegame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasegameComponent
   ],
   imports: [
     BrowserModule,
---
+++ stramers/src/app/basegame/basegame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basegame',
+  templateUrl: './basegame.component.html',
+  styleUrls: ['./basegame.component.css']
+})
+export class BasegameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
