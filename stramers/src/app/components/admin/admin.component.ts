dc1f0b3d70ca64c9d51588cdc121d09d1df8a90e
Terminator_True <jf28renault@gmail.com>
2022-04-04 16:13:42 +0200

    005-Joel

--- stramers/src/app/components/admin/admin.component.ts
+++ stramers/src/app/components/admin/admin.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Global } from 'src/app/services/global';
 //Form
 import { Carta } from 'src/app/models/carta'; 
 
 import { CardService } from 'src/app/services/carta.service';
-
+import { UploadService } from 'src/app/services/upload.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -11,22 +12,41 @@ import { CardService } from 'src/app/services/carta.service';
 })
 export class AdminComponent implements OnInit {
 
-  public cartas: Carta;
+  public carta: Carta;
+  public carta_desada:any;
+  public filesToUpload:any;
 
 
   constructor(
-    private _CardService:CardService
+    private _CardService:CardService,
+    private _uploadService:UploadService
+
 
   ) {
-    this.cartas = new Carta('','','',0,0,0,'','','');
+    this.carta = new Carta('','','',0,0,0,'','','');
   }
 
   ngOnInit() {
   }
-  onSubmit(form:any){
-    this._CardService.saveCard(this.cartas).subscribe(
-      card_data=>console.log(card_data)
-    )
+  fileChangeEvent(fileInput: any){
+    console.log(fileInput)
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
-
+  onSubmit(form:any){
+    this._CardService.saveCard(this.carta).subscribe(
+      result=>{          
+      this.carta_desada=result
+      console.log(result)
+      if( this.carta_desada.project._id !="") {
+          /*this._uploadService.makeFileRequest(
+          Global.url+'/upload-image/'+this.carta_desada._id,
+          [],
+          this.filesToUpload,
+          'image'
+        ).then((res:any) => {
+          console.log(res)
+        });*/
+      }
+      });
+  }
 }
--- stramers/src/app/services/upload.service.ts
+++ stramers/src/app/services/upload.service.ts
@@ -10,17 +10,15 @@ export class UploadService{
         this.url = Global.url;
     }
     //hacemos una peticio AJAX para subir un arxiu al servidor 
-    makeFileRequest(url: string, params: Array<string>, files: Array <any>, name:string){
+    makeFileRequest(url: string, params: Array<string>, files: Array <File>, name:string){
         return new Promise(function(resolve,reject){
-            // crer formulario
             var formData:any=new FormData();
-            //objecto para la peticion AJAX
+
             var xhr=new XMLHttpRequest();
-            //For para recorrer la array
             for (var i=0; i < files.length; i++){
                 formData.append(name, files[i], files[i].name);
             }
-            // la peticion AJAX
+            
             xhr.onreadystatechange = function(){
                 if (xhr.readyState == 4){
                     resolve(JSON.parse(xhr.response));
